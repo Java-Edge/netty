34c64f9660789dba73b39e6b3a39a11120ea826e
JavaEdge <sss2855845850@gmail.com>
2021-05-30 16:44:01 +0800

    feat: ByteBuf 的 duplicate 方法使用

--- example/src/main/java/io/netty/example/helloworld/ByteBufTest.java
+++ example/src/main/java/io/netty/example/helloworld/ByteBufTest.java
@@ -12,28 +12,24 @@ import lombok.extern.slf4j.Slf4j;
 public class ByteBufTest {
 
     public static void main(String[] args) {
-        // 会自动扩容的
-        ByteBuf heapBuffer = UnpooledByteBufAllocator.DEFAULT.heapBuffer(10);
-        heapBuffer.writeByte(1);
-        heapBuffer.writeByte(2);
-        heapBuffer.writeByte(3);
-        heapBuffer.writeByte(4);
-        heapBuffer.writeByte(5);
-        heapBuffer.readByte();
-        heapBuffer.readByte();
+        ByteBuf srcBuf = UnpooledByteBufAllocator.DEFAULT.heapBuffer(10);
+        srcBuf.writeByte(11);
+        srcBuf.writeByte(22);
+        srcBuf.writeByte(33);
+        srcBuf.writeByte(44);
+        srcBuf.writeByte(55);
+        System.out.println(srcBuf.readByte());
+        System.out.println(srcBuf.readByte());
+        System.out.println(srcBuf.readByte());
+        ByteBuf dupBuf = srcBuf.duplicate();
 
-        System.out.println(heapBuffer.readerIndex() + "|" + heapBuffer.writerIndex());
+        srcBuf.writeByte(66);
 
-        ByteBuf sliceBuf = heapBuffer.slice(heapBuffer.readerIndex(), heapBuffer.readableBytes());
-        System.out.println(sliceBuf.readerIndex() + "|" + sliceBuf.writerIndex());
-        System.out.println(sliceBuf.readByte());
-        System.out.println(sliceBuf.writerIndex(4));
-//        heapBuffer.discardReadBytes();
-//        System.out.println(heapBuffer.readerIndex() + "|" + heapBuffer.writerIndex());
-//
-//        ByteBuf directBuffer = UnpooledByteBufAllocator.DEFAULT.directBuffer(10);
-//        System.out.println("refCnt:" + directBuffer.refCnt());
-//        directBuffer.release();
-//        System.out.println("refCnt:" + directBuffer.refCnt());
+        System.out.println(dupBuf.readerIndex() + "|" + dupBuf.writerIndex());
+        dupBuf.writeByte(77);
+
+        System.out.println(srcBuf.readByte());
+        System.out.println(srcBuf.readByte());
+        System.out.println(srcBuf.readByte());
     }
 }
